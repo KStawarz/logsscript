b849f4c0336ee06786d86d3067ad3395255a3953
KStawarz <kstawarz@itsg.com.pl>
2018-10-19 07:31:13 +0200

    add close db connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
-const sourceURL = "mongodb://localhost:27018/MyDb";
-const targetURL = "mongodb://localhost:3001/me";
+// const sourceURL = "mongodb://localhost:27018/MyDb";
+// const targetURL = "mongodb://localhost:3001/me";
+
+const targetURL = "mongodb://localhost:27018/MyDb";
+const sourceURL = "mongodb://localhost:3001/me";
 
 MongoClient.connect(sourceURL, (err, sourceDB) => {
     if(err) throw err;
@@ -8,8 +11,8 @@ MongoClient.connect(sourceURL, (err, sourceDB) => {
     MongoClient.connect(targetURL, (err, targetDB) => {
         if(err) throw err;
 
-        sourceDB.collection('logs', (err, collection) => {
-            sourceDB.collection('logs').find({}).toArray()
+        sourceDB.collection('logs', (err, sourceDBCollection) => {
+            sourceDBCollection.find({}).toArray()
             .then( result => {
                 result.forEach( log => {
                     delete log._id;
@@ -17,11 +20,17 @@ MongoClient.connect(sourceURL, (err, sourceDB) => {
                 })
             })
             .then( () => {
-                    sourceDB.collection('logs').remove({});
+                sourceDBCollection.remove({});
                 })
+            .then( () => {
+                sourceDB.close();
+                targetDB.close();
+            })
             .catch( err=> {
-                    console.log(err);
-                });
+                sourceDB.close();
+                targetDB.close();
+                console.log(err);
+            });
         });
     });
 });
